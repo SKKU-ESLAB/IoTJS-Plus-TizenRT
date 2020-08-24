5bd65e8c588c5ed44304cf2cd848c4663e8feb5d
Gyeonghwan Hong <redcarrottt@gmail.com>
2020-08-24 15:00:48 +0900

    Add iot.js launcher config header file

---
+++ apps/examples/redc_iotjs_launcher/iotjs_launcher_config.h
@@ -0,0 +1,32 @@
+/* Copyright 2019 Gyeonghwan Hong, Sungkyunkwan University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IOTJS_LAUNCHER_CONFIG_H
+#define IOTJS_LAUNCHER_CONFIG_H
+
+#ifndef CONFIG_EXAMPLES_IOTJS_LAUNCHER_WIFI_SSID
+#define CONFIG_EXAMPLES_IOTJS_LAUNCHER_WIFI_SSID "RedCarrottt"
+#endif
+#ifndef CONFIG_EXAMPLES_IOTJS_LAUNCHER_WIFI_PASS
+#define CONFIG_EXAMPLES_IOTJS_LAUNCHER_WIFI_PASS "abc9012abc"
+#endif
+#ifndef CONFIG_EXAMPLES_IOTJS_LAUNCHER_WIFI_AUTH
+#define CONFIG_EXAMPLES_IOTJS_LAUNCHER_WIFI_AUTH WIFI_MANAGER_AUTH_OPEN
+#endif
+#ifndef CONFIG_EXAMPLES_IOTJS_LAUNCHER_WIFI_CRYPTO
+#define CONFIG_EXAMPLES_IOTJS_LAUNCHER_WIFI_CRYPTO WIFI_MANAGER_CRYPTO_NONE
+#endif
+
+#endif /* !defined(IOTJS_LAUNCHER_CONFIG_H) */
